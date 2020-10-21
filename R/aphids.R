fbfd999f337c6286cc807ebca1a3026066351c20
David Warton <dwarton@maths.unsw.edu.au>
2020-10-21 16:59:33 +1100

    add aphids dataset
    add aphids dataset, details still needed

---
+++ R/aphids.R
@@ -0,0 +1,17 @@
+#' Aphid data
+#'
+#' Insert details about the study
+#'
+#' @docType data
+#'
+#' @usage data(aphids)
+#'
+#' @format A list containing whatevs.
+#'
+#' @keywords datasets
+#'
+#' @references insert ref
+#'
+#' @examples
+#' data(aphids)
+"aphids"
