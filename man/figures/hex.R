58b1a46365629253aaf17ee10874416b06a8a176
Fonti Kar <f.kar@unsw.edu.au>
2022-08-26 09:35:52 +1000

    Added files for hex sticker

---
+++ man/figures/hex.R
@@ -0,0 +1,5 @@
+imgurl <- file.path("inst/figures/ecostats.png")
+sticker(imgurl, package=" ", p_size=20,s_x=1.05, s_y=1.02, 
+        s_width=.75, s_height = 0.75,
+        h_fill = "gray24",
+        filename="inst/figures/ecostats_hex.png")
